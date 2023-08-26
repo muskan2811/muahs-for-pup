4bd4262a9bec2c652a6c2b432fa28dcceaac63aa
muskan2811 <83237721+muskan2811@users.noreply.github.com>
2023-08-26 13:45:24 +0530

    Update lovely.py

--- lovely.py
+++ lovely.py
@@ -15,6 +15,10 @@ if lovey == "So much":
   st.write("Soooo much")
   st.write("Sooooo much")
   st.write("Soooooo much")
+elif lovey == "":
+  st.write("Say something! Anything")
+elif lovey == "Something" or "something" or "anything" or "Anything":
+  st.write("Schmuh cutie! Why are you like this? Aise banoge head?")
 else:
   st.write("Nuh uh! WRONG!")
   st.write("I love you so much")
